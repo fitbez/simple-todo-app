4c7c7f5019ed08a18ae8a33120cde9b57f2fe911
Fitsum <fitbez@gmail.com>
2020-07-19 11:49:31 -0400

    Updates

---
+++ precache-manifest.5e82ca08377075d222fd8b43e194c79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf93d464c30a09a599649c87bed61c6",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "65b3a61e939cd40c01ab",
+    "url": "/simple-todo-app/static/css/main.e67f349a.chunk.css"
+  },
+  {
+    "revision": "821f6e93be0864002b4b",
+    "url": "/simple-todo-app/static/js/2.5c5e8f56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.5c5e8f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b3a61e939cd40c01ab",
+    "url": "/simple-todo-app/static/js/main.48e1a5ba.chunk.js"
+  },
+  {
+    "revision": "e165d461a30ce2094b0e",
+    "url": "/simple-todo-app/static/js/runtime-main.065a362d.js"
+  }
+]);
